84add3379bacae08f3c4f9af4a4bc817bcbce3b7
Felipe Muniz <muniz.r.felipe@gmail.com>
2019-10-05 06:47:36 -0300

    Documentação moderada adicionada

--- src/numerico/linalg/core.py
+++ src/numerico/linalg/core.py
@@ -6,9 +6,11 @@ from .cholesky import Cholesky as _Cholesky
 
 
 def square(a):
-    return a.shape[0] == a.shape[1]
+    '''Testa se um array é quadrado.'''
+    return a.ndim == 2 and a.shape[0] == a.shape[1]
 
 def simmetrical(a):
+    '''Testa se uma matriz é simétrica fazendo sua transposição.'''
     return (a == a.T).all()
 
 def lu(a, *args, **kwargs):
@@ -20,19 +22,33 @@ def cholesky(a, *args, **kwargs):
     return dec(a)
 
 def is_lower_trig(a):
+    '''Testa se uma matriz é triangular inferior.
+    Complexidade: ~n²/2 comparações (pior caso)'''
     for i in range(a.shape[0]):
         for j in range(i + 1, a.shape[1]):
             if a[i,j] != 0: return False
     return True
 
 def is_upper_trig(a):
+    '''Testa se uma matriz é triangular superior.
+    Complexidade: ~n²/2 comparações (pior caso)'''
     for i in range(a.shape[0]):
         for j in range(i):
             if a[i,j] != 0: return False
     return True
 
-def successive_substitutions(a, b):
-    if not is_lower_trig(a):
+def successive_substitutions(a, b, skip_check=False):
+    '''
+    Faz substituições sucessivas em uma matriz escalonada triangular inferior.
+    a: matriz triangular inferior
+    b: vetor de coeficientes independentes
+
+    Passe skip_check=True se tiver certeza de que a é triangular inferior, 
+    pulando o teste de complexidade ~n²/2.
+
+    Complexidade: ~n²'''
+
+    if not skip_check and not is_lower_trig(a):
         raise ValueError('a must be a lower trig matrix.')
     b = b.copy()
     x = np.zeros_like(b)
@@ -40,8 +56,18 @@ def successive_substitutions(a, b):
         x[i] = (b[i] - (x[:i] * a[i, :i]).sum()) / a[i, i]
     return x
 
-def retroactive_substitutions(a, b):
-    if not is_upper_trig(a):
+def retroactive_substitutions(a, b, skip_check=False):
+    '''
+    Faz substituições retroativas em uma matriz escalonada triangular superior.
+    a: matriz triangula superiorr
+    b: vetor de coeficientes independentes
+
+    Passe skip_check=True se tiver certeza de que a é triangular superior, 
+    pulando o teste de complexidade ~n²/2.
+
+    Complexidade: ~n²'''
+
+    if not skip_check and not is_upper_trig(a):
         raise ValueError('a must be an upper trig matrix.')
     b = b.copy()
     x = np.zeros_like(b)
@@ -50,9 +76,10 @@ def retroactive_substitutions(a, b):
     return x
 
 def det(a):
+    '''Retorna o determinante da matriz a, usando a decomposição mais adequada.'''
     try:
         if is_lower_trig(a) or is_upper_trig(a):
-            a.diagonal().prod()
+            return a.diagonal().prod()
         elif simmetrical(a):
             l = cholesky(a)
             return l.diagonal().prod()**2
@@ -64,6 +91,8 @@ def det(a):
         return 0
 
 def decomp(a):
+    '''Testa se a é simétrica; se for, decompõe usando cholesky; c.c., decompõe
+    usando LU.'''
     if not square(a): raise ValueError('a must be a square matrix.')
     if simmetrical(a):
         l = cholesky(a)
@@ -74,25 +103,29 @@ def decomp(a):
         l, u, p, sign = lu(a)
     return l, u, p, sign
 
-def quicksolve(l, u, b):
+def quicksolve(l, u, b, *args, **kwargs):
+    '''Usa substituições retroativas e sucessivas para resolver um sistema do 
+    tipo LUx = b, onde L é triangular inferior e U é triangular superior.'''
     if not u.shape == l.shape:
         raise ValueError('L and U must have the same shape.')
-    y = successive_substitutions(l, b)
-    x = retroactive_substitutions(u, y)
+    y = successive_substitutions(l, b, *args, **kwargs)
+    x = retroactive_substitutions(u, y, *args, **kwargs)
     return x
 
 def solve(a, b):
+    '''Decompõe a matriz a em uma triangular superior e outra inferior, e 
+    resolve o sistema LUx = pb onde LU = pA.'''
     l, u, p, sign = decomp(a)
-    return quicksolve(l, u, p@b)
+    return quicksolve(l, u, p@b, skip_check=True)
 
 def inv(a):
+    '''Usa as decomposições LU ou cholesky para achar a inversa da matriz a.'''
     try:
-        l, u, p, sign = decomp(a)
+        l, u, e, sign = decomp(a)
     except ZeroDivisionError:
         raise ZeroDivisionError('Can\'t invert a singular matrix (det = 0).')
     n = len(a)
-    e = p @ np.identity(n)
     out = []
     for i in range(n):
-        out.append(quicksolve(l, u, e[:, i]))
+        out.append(quicksolve(l, u, e[:, i], skip_check=True))
     return np.array(out).T
